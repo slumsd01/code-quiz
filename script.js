7f84713d391a313155516c76dbab132db7ac0f69
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-10-29 06:59:13 -0400

    fixed errors

--- script.js
+++ script.js
@@ -39,91 +39,66 @@ let questionarray = [
         correctanswer: "Both the <head> section and the <body> section are correct"
     },
     question3 = {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choices: ["<script name = 'xxx.js'>" , "<script href='xxx.js'> ", "<script src='xxx.js'>" , "<script a='xxx.js'>"],
-        correctanswer: "<script src='xxx.js'>"
-    },
-    question4 = {
-        question: "How do you write 'Hello World!' in an alert box?",
-        choices: ["msgBox('Hello World!')" , "msg('Hello World!')", "alertBox('Hello World!')" , "alert('Hello World!')"],
-        correctanswer: "alert('Hello World');"
-    },
-    question5 = {
         question: "How do you create a function in JavaScript?",
         choices: ["function:myFunction()" , "function = myFunction()", "function myFunction()" , "let myFunction = ()"],
         correctanswer: "function myFunction()" 
     },
-    question6 = {
+    question4 = {
         question: "How do you call a function named 'myFunction'?",
         choices: ["call myFunction()" , "myFunction()", "call function myFunction()" , "call(myFunction)"],
         correctanswer: "myFunction()"
     },
-    question7 = {
-        question: "How to write an IF statement in JavaScript?'?",
+    question5 = {
+        question: "How to write an IF statement in JavaScript?",
         choices: ["if i = 5 then" , "if i = 5", "if (i == 5)" , "if i == 5 then"],
         correctanswer: "if (i == 5)"
     },
-    question8 = {
+    question6 = {
         question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
         choices: ["if (i != 5)" , "if i <> 5", "if (i <> 5)" , "if i =! 5 then"],
         correctanswer: "if (i != 5)"
     },
-    question9 = {
+    question7 = {
         question: "How does a WHILE loop start?",
         choices: ["while (i <= 10)" , "while (i <=10; 10++)", "while i = 1 to 10" , "while i++ = 10"],
         correctanswer: "while (i <= 10)"
     },
-    question10 = {
+    question8 = {
         question: "How does a FOR loop start?",
         choices: ["for i = 1 to 5" , "for (i < = 5; i++", "for (i = 0; i <= 5; i++)" , "for (i = 0; i <= 5)"],
         correctanswer: "for (i = 0; i <= 5; i++)"
     },
-    question11 = {
-        question: "How can you add a comment in a JavaScript?",
-        choices: ["<!--This is a comment-->" , "'This is a comment", "//This is a comment" , "[[This is a comment]]"],
-        correctanswer: "//This is a comment"
-    },
-    question12 = {
-        question: "What is the correct way to write a JavaScript array?",
-        choices: ["var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')" , "var colors = ['red', 'green', 'blue']", "var colors = 'red', 'green', 'blue'" , "var colors = (1:'red', 2:'green', 3:'blue'"],
-        correctanswer: "var colors = ['red', 'green', 'blue']"
-    },
-    question13 = {
-        question: "How do you round the number 7.25, to the nearest integer?",
-        choices: ["rnd(7.25)" , "Math.round(7.25)", "round(7.25)" , "Math.rnd(7.25)"],
-        correctanswer: "Math.round(7.25)"
-    },
-    question14 = {
+    question9 = {
         question: "How do you find the number with the highest value of x and y?",
         choices: ["top(x,y)" , "Math.max(x,y)", "ceil(x,y)" , "Math.ceil(x,y)"],
         correctanswer: "Math.max(x,y)"
     },
-    question15 = {
+    question10 = {
         question: "Which event occurs when the user clicks on an HTML element?",
         choices: ["onclick" , "onchange", "onmouseover" , "onmouseclick"],
         correctanswer: "onclick"
     },
-    question16 = {
+    question11 = {
         question: "How do you declare a JavaScript variable?",
         choices: ["v carName;" , "variable carName;", "var carName;" , "variant carName;"],
         correctanswer: "var carName;"
     },
-    question17 = {
+    question12 = {
         question: "Which operator is used to assign a value to a variable?",
         choices: ["=" , "x", "*" , "-"],
         correctanswer: "="
     },
-    question18 = {
+    question13 = {
         question: "What will the code return? Boolean(3 < 7)",
         choices: ["true" , "false", "NaN" , "SyntaxError"],
         correctanswer: "true"
     },
-    question19 = {
+    question14 = {
         question: "A JavaScript file has an extension of:",
         choices: [".Java" , ".js", ".javascript" , ".xml"],
         correctanswer: ".js"
     },
-    question20 = {
+    question15 = {
         question: "Which dialog box type displays a message and a data entry field?",
         choices: ["alert()" , "prompt()", "confirm()" , "msg()"],
         correctanswer: "prompt()"
@@ -185,6 +160,11 @@ let initialinput;
 
 function endgame() {
     clearInterval(timerinterval);
+    if (seconds < 0) {
+        seconds = 0;       
+    }
+
+    document.getElementById('quizquestions').hidden = true;
     document.getElementById('finish').hidden = false;
     finalscoreEl.textContent = seconds;
 }
